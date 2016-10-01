69c335c5a0e5d07f7f4c288f71ead3f96525d176
Unnamed999999999 <Unnamed999999999@Gmail.com>
2016-10-01 19:17:19 +0300

    Libs and styles added

--- PrintIt_Desktop_4/MainWindow.xaml.cs
+++ PrintIt_Desktop_4/MainWindow.xaml.cs
@@ -11,13 +11,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace PrintIt_Desktop_4
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
