05e79c9de50f67a14fae2cc2664363cfc9c31a0a
Unnamed999999999 <Unnamed999999999@Gmail.com>
2016-10-10 22:03:48 +0300

    added SignUp prototype and UnitTest project sceleton

---
+++ PrintIt_Desktop_4.UnitTests/LoginAndSignUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrintIt_Desktop_4.UnitTests
+{
+    [TestClass]
+    public class LoginAndSignUp
+    {
+        /*
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+         */
+    }
+}
