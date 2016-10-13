772a9043399a36a38cfed3b5bc0565644f90c14c
Unnamed999999999 <Unnamed999999999@Gmail.com>
2016-10-13 19:05:35 +0300

    added library for Commands

--- PrintIt_Desktop_4/Views/LoadAndLoagin.xaml.cs
+++ PrintIt_Desktop_4/Views/LoadAndLoagin.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -84,8 +86,40 @@ namespace PrintIt_Desktop_4.Views
             ResizeMode = ResizeMode.CanMinimize;
             WindowCloseButtonStyle = Application.Current.FindResource("DarkCloseWindowButtonStyle") as Style;
             WindowMinButtonStyle = Application.Current.FindResource("DarkMinMaxWindowButtonStyle") as Style;
+
+            //SendTestRequest();
+            //SendTestRequest2();
         }
 
+        private void SendTestRequest()
+        {
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["email"] = "test@test.com";
+                values["password"] = "12345678";
+
+                var response = client.UploadValues("http://ec2-52-57-154-202.eu-central-1.compute.amazonaws.com/api/v1/sign_in", values);
+
+                var responseString = Encoding.Default.GetString(response);
+                MessageBox.Show(responseString);
+            }
+        }
+
+        private void SendTestRequest2()
+        {
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["email"] = "fail@test.com";
+                values["password"] = "123456";
+
+                var response = client.UploadValues("http://ec2-52-57-154-202.eu-central-1.compute.amazonaws.com/api/v1/sign_in", values);
+
+                var responseString = Encoding.Default.GetString(response);
+                MessageBox.Show(responseString);
+            }
+        }
 
     }
 }
