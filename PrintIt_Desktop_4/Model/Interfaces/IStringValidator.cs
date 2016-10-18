836118c47a607bb9493fd7af005bb084ae3c33e9
Unnamed999999999 <Unnamed999999999@Gmail.com>
2016-10-19 00:37:22 +0300

    fix

---
+++ PrintIt_Desktop_4/Model/Interfaces/IStringValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrintIt_Desktop_4.Model.Interfaces
+{
+        public interface IStringValidator<T> where T : struct
+        {
+            T[] Validate(String data);
+        }
+}
